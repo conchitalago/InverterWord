c2d6ccee2b61c0cc495e809e51b491fc6af0eae8
tdom91 <tomasdominik@outlook.com>
2021-09-09 15:47:03 +0200

    Agregar archivos de proyecto.

---
+++ InverterWord/InverterModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InverterWord
+{
+    public class InverterModel
+    {
+        public static string InvertWord(string word)
+        {
+            string result = "";
+            if (word.Length == 0)//Si la palabra solo tiene un caracter devuelve la misma palabra
+            {
+                result = word;
+            }
+            else
+            {
+                result = InvertWord(word.Substring(1)) + word[0];
+            }
+            return result;
+        }
+    }
+}
---
+++ InverterWord/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+namespace InverterWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduce la palabra:");
+            string wordchoose = Console.ReadLine();
+            string wordinverter = InverterModel.InvertWord(wordchoose);
+            Console.WriteLine(wordinverter);
+        }
+    }
+}
